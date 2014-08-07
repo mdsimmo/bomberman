6ea81c146df2a819487a1d139e0b436f1e307b6b
Mark Smullen <marktexnical@gmail.com>
2014-08-07 03:35:12 -0500

    Add missed Sudden Death counter from merge

---
+++ src/main/java/io/github/mdsimmo/bomberman/SuddenDeathCounter.java
@@ -0,0 +1,60 @@
+package io.github.mdsimmo.bomberman;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+
+public class SuddenDeathCounter implements Runnable {
+
+	private static Plugin plugin = Bomberman.instance;
+	private Game game;
+	int timeout;
+	int suddenDeath;
+	int taskId;
+	
+	public SuddenDeathCounter(Game game) {
+		this.game = game;
+		taskId = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this, 0, 20);
+		timeout = game.getTimeout();
+		suddenDeath = game.getSuddenDeath();
+	}
+	
+	@Override
+	public void run() {
+		if (!game.isPlaying)
+			plugin.getServer().getScheduler().cancelTask(taskId);
+			
+		timeout--;
+		suddenDeath--;
+		if (timeout == 30)
+		    game.sendMessage(game.players, "Game over in "
+                    + ChatColor.YELLOW + timeout + ChatColor.WHITE
+                    + " seconds!");
+		if (timeout == 10)
+		    game.sendMessage(game.observers, "Game over in "
+                    + ChatColor.YELLOW + timeout + ChatColor.WHITE
+                    + " seconds!");
+		else if (timeout < 10 && timeout > 0)
+		    game.sendMessage(game.players, "" + timeout);
+		else if (timeout == 0) {
+		    game.sendMessage(game.observers, "Game over!");
+			game.terminate();
+		}
+		
+		if (suddenDeath == 30)
+            game.sendMessage(game.players, "Sudden death in "
+                    + ChatColor.YELLOW + suddenDeath + ChatColor.WHITE
+                    + " seconds!");
+		
+		if (suddenDeath <= 10) {
+		    if (suddenDeath == 10)
+                game.sendMessage(game.observers, "Sudden death in " + ChatColor.YELLOW
+                        + suddenDeath + ChatColor.WHITE + " seconds!");
+		    else if (suddenDeath > 0) {
+		        game.sendMessage(game.players, "" + suddenDeath);
+		    } else {
+		        game.sendMessage(game.observers, ChatColor.RED + "Sudden death!");
+	            game.setSuddenDeath(true);
+		    }
+		}
+	}
+}
