43e1bc9fe2770bb6a27e5aadec87124ba1dba2b8
Mark Smullen <marktexnical@gmail.com>
2014-08-08 04:31:22 -0500

    Moved plugin messaging helper commands

--- src/main/java/io/github/mdsimmo/bomberman/Bomberman.java
+++ src/main/java/io/github/mdsimmo/bomberman/Bomberman.java
@@ -1,5 +1,10 @@
 package io.github.mdsimmo.bomberman;
 
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Bomberman extends JavaPlugin {
@@ -13,6 +18,26 @@ public class Bomberman extends JavaPlugin {
 	
 	/* TODO BUGS
 	 */
+	
+	public static void sendMessage(Player[] playerList, String message) {
+        for(Player p : playerList) {
+            sendMessage(p, message);
+        }
+    }
+    
+    public static void sendMessage(ArrayList<PlayerRep> playerList, String message) {
+        for(PlayerRep p : playerList) {
+            sendMessage(p.player, message);
+        }
+    }
+    
+    public static void sendMessage(Player p, String message) {
+        p.sendMessage(ChatColor.GREEN + "[BomberMan] " + ChatColor.WHITE + message);
+    }
+    
+    public static void sendMessage(CommandSender sender, String message) {
+        sender.sendMessage(ChatColor.GREEN + "[BomberMan] " + ChatColor.WHITE + message);
+    }
 	
 	@Override
 	public void onEnable() {
