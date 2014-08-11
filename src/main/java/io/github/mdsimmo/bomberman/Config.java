6846d9e5d754510bc8f463420b016badc5a8ace1
Mark Smullen <marktexnical@gmail.com>
2014-08-11 05:01:34 -0500

    Fix autostart config option

--- src/main/java/io/github/mdsimmo/bomberman/Config.java
+++ src/main/java/io/github/mdsimmo/bomberman/Config.java
@@ -21,7 +21,7 @@ public enum Config {
 	BOMBS ( "bombs", 3),
 	POWER ( "power", 3),
 	MIN_PLAYERS ( "minplayers", 2),
-	AUTOSTART ( "autostart", false ),
+	AUTOSTART ( "autostart.enabled", false ),
 	AUTOSTART_DELAY ( "autostart.delay", 30 ),
 	DEFAULT_ARENA ("defaultarena", "default" ),
 	DROPS_ITEMS ("drops.items", Arrays.asList(
