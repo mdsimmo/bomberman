96bebd8701c65d0e70d24b65b18db444cffc0778
mdsimmo <mdsimmo2+git@gmail.com>
2015-08-29 11:35:14 +1000

    improved the Language class

--- src/main/java/com/github/mdsimmo/bomberman/localisation/Language.java
+++ src/main/java/com/github/mdsimmo/bomberman/localisation/Language.java
@@ -1,93 +1,121 @@
 package com.github.mdsimmo.bomberman.localisation;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.util.*;
-
 import com.github.mdsimmo.bomberman.Bomberman;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Language implements Formattable {
 
-    private static Map<String, Language> langs = new HashMap<String, Language>();
-    private static Plugin plugin = Bomberman.instance();
-    private static File langsDir = new File( plugin.getDataFolder(), "languages" );
+    public static class LanguageNotFoundException extends Exception {
+        public LanguageNotFoundException( String s, Throwable throwable ) {
+            super( s, throwable );
+        }
+
+    }
+
+    private static final Map<String, Language> langs = new HashMap<String, Language>();
+    private static final Plugin plugin = Bomberman.instance();
+    private static final File languagesDir = new File( plugin.getDataFolder(), "languages" );
+
+    static {
+        langs.put( "english", EnglishLanguage.instance );
+    }
 
     /**
-     * Gets the language with the given name. If the language does not exist, then the english language will be
-     * returned. Thus, this method will always return a valid language.
-     * @param lang the language name to get
-     * @return the language to use for that name. Never null
+     * Gets the language with the given name. If the language does not exist, then a FileNotFoundException will be thrown.
+     * @param name the name of the language to fetch
+     * @return the language to use for that name. Never null.
+     * @throws NullPointerException if name is null
+     * @throws FileNotFoundException if there is no language file for the requested file
      */
-    public static Language getLanguage( String lang ) {
-        if ( lang == null )
-            return null;
-        lang = lang.toLowerCase();
-        Language language = langs.get( lang );
-        if ( language != null ) {
+    public static Language getLanguage( String name ) throws FileNotFoundException {
+        if ( name == null )
+            throw new NullPointerException( "name cannot be null" );
+
+        String matchName = name.toLowerCase();
+        // first check in cache
+        Language language = langs.get( matchName );
+        if ( language != null )
             return language;
-        } else {
-            if ( getFile( lang ).exists() ) {
-                language = new Language( lang );
-                langs.put( lang, language );
-                return language;
-            } else {
-                if ( lang.equalsIgnoreCase( "english" ) ) {
-                    language = new EnglishLanguage();
-                    langs.put( "english", language );
-                    return language;
-                } else {
-                    return null;
-                }
-            }
-        }
+
+        // attempt to get from file
+        File file = getFile( name );
+        language = loadLanguage( name, file );
+
+        // cache and return the result
+        langs.put( matchName, language );
+        return language;
     }
 
     /**
-     * Tests if the language with the given name exists.
-     * @param lang the languages name. "English" or an empty string for english
-     * @return true if the language exists
+     * Looks up the file that the given language will be stored in.
+     * @param name the name of the language to look for
+     * @return the languages location
+     * @throws FileNotFoundException
      */
-    public static boolean hasLanguage( String lang ) {
-        lang = lang.toLowerCase();
-        return lang.equals("english") || lang.isEmpty() || lang.contains(lang);
+    private static File getFile( String name ) throws FileNotFoundException {
+        return new File( languagesDir, name );
     }
 
-    private static File getFile( String lang ) {
-        return new File( plugin.getDataFolder(), lang.toLowerCase() + ".lang" );
+    /**
+     * Loads the given language from a file
+     * @param name the name of the language to load
+     * @param file the file the languages data is stored in
+     * @return the loaded language
+     */
+    private static Language loadLanguage( String name, File file ) {
+        if ( file == null )
+            throw new NullPointerException( "file cannot be null" );
+        if ( !file.exists() )
+            throw new IllegalArgumentException( "File must exist" );
+        YamlConfiguration languageConfig = YamlConfiguration.loadConfiguration( file );
+        return new Language( name, languageConfig );
     }
 
-    public static List<String> allLanguages() {
-        File dir = plugin.getDataFolder();
-        File[] files = dir.listFiles( new FilenameFilter() {
-            @Override
-            public boolean accept( File file, String name ) {
-                return name.endsWith( ".lang" );
-            }
-        } );
-        List<String> langs = new ArrayList<String>( files.length );
-        for ( File f : files ) {
-            langs.add( f.getName() );
-        }
-        if ( !langs.contains( "english" ) )
-            langs.add( "english" );
-        return langs;
+    /**
+     * Gets the default language to use. This language can be used as a backup to other languages.
+     * @return the default language - always a valid language and never null
+     */
+    public static Language getDefaultLanguage() {
+        // todo read default language from config
+        return EnglishLanguage.instance;
     }
 
     private final YamlConfiguration save;
     private final String name;
     private final Language bup;
 
-    private Language( String lang ) {
-        File f = getFile( lang );
-        save = YamlConfiguration.loadConfiguration( f );
-        name = lang;
-        String bupLang = save.getString( "language-bup" );
-        if ( bupLang != null )
-            bup = getLanguage( bupLang );
-        else
-            bup = getLanguage( "english" );
+    private Language( String name, YamlConfiguration save ) {
+        this.name = name;
+        this.save = save;
+        this.bup = getBup();
+    }
+
+    Language getBup() {
+        String bupName = save.getString( "backup-language" );
+        Language bup = null;
+        if ( bupName == null ) {
+            bup = getDefaultLanguage();
+        } else {
+            try {
+                bup = getLanguage( name );
+            } catch ( FileNotFoundException e ) {
+                bup = getDefaultLanguage();
+                plugin.getLogger().warning( "language " + name + " had an invalid backup language " + bupName + ". Using " + bup.name + "." );
+            }
+        }
+        return bup;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 
     /**
@@ -97,14 +125,10 @@ public class Language implements Formattable {
      * @return the translated phrase.
      */
     public String translate( Phrase phrase ) {
-        String t = save.getString(phrase.getKey());
+        String t = save.getString( phrase.getKey() );
         if ( t == null )
-            Language bup = getLanguage( (String)Config.LANGUAGE.getValue( save ) );
-            if ( bup == null )
-                return phrase.getDefault();
-            else
-                return bup.translate( phrase );
-        } else
+            return bup.translate( phrase );
+        else
             return t;
     }
 
@@ -117,8 +141,16 @@ public class Language implements Formattable {
      * The default english language. Simply takes all it's values from the phrases default value
      */
     private static class EnglishLanguage extends Language {
+
+        public static final EnglishLanguage instance = new EnglishLanguage();
+
         public EnglishLanguage() {
-            super( "english" );
+            super( "english", null  );
+        }
+
+        @Override
+        Language getBup() {
+            return null;
         }
 
         public String translate( Phrase phrase ) {
