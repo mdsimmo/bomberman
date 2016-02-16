dde05545092d36cc1469acf516e8f4ff979dd988
mdsimmo <mdsimmo2+git@gmail.com>
2016-02-16 20:23:47 +1100

    fixed nullpointer on no game
    GameCommand was not checking if the player had previously interacted with a game. This resulted in a null pointer the first time the player did something

--- src/main/java/io/github/mdsimmo/bomberman/commands/GameCommand.java
+++ src/main/java/io/github/mdsimmo/bomberman/commands/GameCommand.java
@@ -14,85 +14,89 @@ import org.bukkit.entity.Player;
 
 public abstract class GameCommand extends Cmd {
 
-	@Override
-	public Message name( CommandSender sender ) {
-		return getMessage( nameShort(), sender );
-	}
-	
-	public abstract Phrase nameShort();
-	
-	public GameCommand( Cmd parent ) {
-		super( parent );
-	}
+    @Override
+    public Message name( CommandSender sender ) {
+        return getMessage( nameShort(), sender );
+    }
+    
+    public abstract Phrase nameShort();
+    
+    public GameCommand( Cmd parent ) {
+        super( parent );
+    }
 
-	public final List<String> options( CommandSender sender, List<String> args ) {
-		if ( args.size() <= 1 ) {
-			List<String> list = new ArrayList<>( Game.allGames() );
-			if ( sender instanceof Player
-					&& PlayerRep.getPlayerRep( (Player)sender ).getActiveGame() != null ) {
-				List<String> options = shortOptions( sender, args );
-				if ( options != null )
-					list.addAll( options );
-			}
-			return list;
-		} else {
-			args.remove( 0 );
-			return shortOptions( sender, args );
-		}
-	}
+    public final List<String> options( CommandSender sender, List<String> args ) {
+        if ( args.size() <= 1 ) {
+            List<String> list = new ArrayList<>( Game.allGames() );
+            if ( sender instanceof Player
+                    && PlayerRep.getPlayerRep( (Player)sender ).getActiveGame() != null ) {
+                List<String> options = shortOptions( sender, args );
+                if ( options != null )
+                    list.addAll( options );
+            }
+            return list;
+        } else {
+            args.remove( 0 );
+            return shortOptions( sender, args );
+        }
+    }
 
-	public abstract List<String> shortOptions( CommandSender sender,
-			List<String> args );
+    public abstract List<String> shortOptions( CommandSender sender,
+            List<String> args );
 
-	@Override
-	public final boolean run( CommandSender sender, List<String> args ) {
-		Game game;
-		if ( args.size() >= 1 && Game.allGames().contains( args.get( 0 ) ) ) {
-			game = Game.findGame( args.get( 0 ) );
-			if ( sender instanceof Player )
-				PlayerRep.getPlayerRep( (Player)sender ).setActiveGame( game );
-			args.remove( 0 );
-		} else {
-			if ( sender instanceof Player )
-				game = PlayerRep.getPlayerRep( (Player)sender ).getActiveGame();
-			else
-				return false;
-		}
-		return runShort( sender, args, game );
-	}
+    @Override
+    public final boolean run( CommandSender sender, List<String> args ) {
+        Game game;
+        if ( args.size() >= 1 && Game.allGames().contains( args.get( 0 ) ) ) {
+            // game was specified so use that
+            game = Game.findGame( args.get( 0 ) );
+            if ( sender instanceof Player )
+                PlayerRep.getPlayerRep( (Player)sender ).setActiveGame( game );
+            args.remove( 0 );
+        } else {
+            // try to get the game from what the user last did
+            if ( sender instanceof Player ) {
+                game = PlayerRep.getPlayerRep( (Player) sender ).getActiveGame( );
+                if ( game == null )
+                    return false;
+            } else
+                return false;
+        }
+        return runShort( sender, args, game );
+    }
 
-	public abstract boolean runShort( CommandSender sender, List<String> args,
-			Game game );
+    public abstract boolean runShort( CommandSender sender, List<String> args,
+            Game game );
 
-	@Override
-	public Message extra( CommandSender sender ) {
-		return getMessage( extraShort(), sender );
-	}
+    @Override
+    public Message extra( CommandSender sender ) {
+        return getMessage( extraShort(), sender );
+    }
 
-	public abstract Phrase extraShort();
+    public abstract Phrase extraShort();
 
-	@Override
-	public Message example( CommandSender sender ) {
-		String game = Utils.random( Game.allGames() );
-		if ( game == null )
-			game = "mygame";
-		return getMessage( exampleShort(), sender ).put( "example", game );
-	}
+    @Override
+    public Message example( CommandSender sender ) {
+        String game = Utils.random( Game.allGames() );
+        if ( game == null )
+            game = "mygame";
+        return getMessage( exampleShort(), sender ).put( "example", game );
+    }
 
-	public abstract Phrase exampleShort();
+    public abstract Phrase exampleShort();
 
-	@Override
-	public Message description( CommandSender sender ) {
-		return getMessage( descriptionShort(), sender );
-	}
+    @Override
+    public Message description( CommandSender sender ) {
+        return getMessage( descriptionShort(), sender );
+    }
 
-	public abstract Phrase descriptionShort();
+    public abstract Phrase descriptionShort();
 
-	@Override
-	public Message usage( CommandSender sender ) {
-		return getMessage( usageShort(), sender );
-	}
+    @Override
+    public Message usage( CommandSender sender ) {
+        return getMessage( usageShort(), sender );
+    }
 
-	public abstract Phrase usageShort();
+    public abstract Phrase usageShort();
 
 }
