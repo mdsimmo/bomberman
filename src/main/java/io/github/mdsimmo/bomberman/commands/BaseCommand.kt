72194048135d81976f3f2cd83fa383e9524f08e6
BuildTools <unconfigured@null.spigotmc.org>
2020-04-20 23:42:00 +1000

    Disabled DevInfo

--- src/main/java/io/github/mdsimmo/bomberman/commands/BaseCommand.kt
+++ src/main/java/io/github/mdsimmo/bomberman/commands/BaseCommand.kt
@@ -14,6 +14,7 @@ class BaseCommand : CommandGroup(null), TabCompleter, CommandExecutor {
 
     init {
         addChildren(
+                //DevInfo(this),
                 Set(this),
                 GameCreate(this),
                 GameInfo(this),
@@ -23,8 +24,7 @@ class BaseCommand : CommandGroup(null), TabCompleter, CommandExecutor {
                 RunStart(this),
                 RunStop(this),
                 GameList(this),
-                GameReload(this),
-                DevInfo(this)
+                GameReload(this)
         )
     }
 
